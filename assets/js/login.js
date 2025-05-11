1feb2d4a1cc09d7d56b7b6a191bb639c304115fd
Fagner <fagnernezim@gmail.com>
2025-05-11 19:56:45 -0300

    update

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCHdlxtVrVbly3_Hy6ewhgUMwV45OxJE7g",
-  authDomain: "equipamentos-e825a.firebaseapp.com",
-  projectId: "equipamentos-e825a",
-  storageBucket: "equipamentos-e825a.firebasestorage.app",
-  messagingSenderId: "415306494134",
-  appId: "1:415306494134:web:cad2d026744535f21d3617",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 function fazerLogin() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCHdlxtVrVbly3_Hy6ewhgUMwV45OxJE7g",
+    authDomain: "equipamentos-e825a.firebaseapp.com",
+    projectId: "equipamentos-e825a",
+    storageBucket: "equipamentos-e825a.firebasestorage.app",
+    messagingSenderId: "415306494134",
+    appId: "1:415306494134:web:cad2d026744535f21d3617",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
   const email = document.getElementById("usuario").value;
   const senha = document.getElementById("senha").value;
 
@@ -27,3 +28,8 @@ function fazerLogin() {
       alert("Erro ao autenticar. Verifique suas credenciais.");
     });
 }
+
+document.getElementById("btn-login").addEventListener("click", function (e) {
+  e.preventDefault();
+  fazerLogin();
+});
