a48eb2d185a718cc0d122b501ad83c4da5c70435
Fagner <fagnernezim@gmail.com>
2025-05-11 19:41:57 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,16 +13,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+function fazerLogin() {
+  const email = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
 
-const email = document.getElementById("usuario").value;
-const senha = document.getElementById("senha").value;
-
-signInWithEmailAndPassword(email, senha)
-  .then((userCredential) => {
-    console.log("Login realizado com sucesso!", userCredential.user);
-    alert("Login bem-sucedido!");
-  })
-  .catch((error) => {
-    console.error("Erro ao fazer login:", error.message);
-    alert("Erro ao autenticar. Verifique suas credenciais.");
-  });
+  signInWithEmailAndPassword(email, senha)
+    .then((userCredential) => {
+      console.log("Login realizado com sucesso!", userCredential.user);
+      alert("Login bem-sucedido!");
+    })
+    .catch((error) => {
+      console.error("Erro ao fazer login:", error.message);
+      alert("Erro ao autenticar. Verifique suas credenciais.");
+    });
+}
